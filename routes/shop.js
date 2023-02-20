ef21eac8cdfe31be2f298f35c4c48cdb7ad532bc
Urs-Praveen <work.erpraveen@gmail.com>
2023-02-20 15:39:16 +0530

    routes with separate route filters

---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const routes = express.Router()
+
+
+routes.get('/',(req,res,next)=>{    
+    res.send('<h1> Welcome To Express!!</h1>')
+})
+
+module.exports =routes;
--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express')
-
 const bodyParser = require('body-parser');
-
 const app = express();
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res)=>{
-    console.log('in the middleware')
-    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>')
-    res.send('<h1> Add product page!!</h1>')
-    
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
-app.get('/product',(req,res,next)=>{
-    console.log(req.body)
-    
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    console.log('in another middleware')
-    res.send('<h1> Welcome To Express!!</h1>')
-})
-
+ 
 app.listen(4000)
